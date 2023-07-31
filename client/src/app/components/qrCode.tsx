98e13367ab7d8a21f44fc64f2f46ac180a0a8196
Ilya <officalbatya@gmail.com>
2023-07-31 18:22:57 +0300

    support github.com/0xPolygonID/extension-demo

--- client/src/app/components/qrCode.tsx
+++ client/src/app/components/qrCode.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import QRCode from 'qrcode.react';
-import { Box } from "theme-ui";
+import { Container, Box, Button} from '@mui/material';
 
 interface CodeProps {
     value: string;
@@ -9,10 +9,27 @@ interface CodeProps {
 const Code: FunctionComponent<CodeProps> = (props) => {
   const { value } = props;
   return (
-    <Box sx={{ width: [400] }}>
-      <QRCode level={"L"} size={400} value={value} />
-    </Box>
+    <Container>
+      <Box sx={{ width: [400] }}>
+        <QRCode level={"L"} size={400} value={value} />
+      </Box>
+      <Button onClick={() => dispachEvent(value)} 
+        variant="contained" size="large" sx={{
+          width: "100%",
+          marginTop: "15px"
+        }}> Polygon ID </Button>
+    </Container>
   );
 };
+
+const dispachEvent = async (value: string) => {
+  console.log('data to ext:', value);
+  const msg = btoa(value);
+  const hrefValue = `iden3comm://?i_m=${msg}`;
+  console.log('link to ext:', hrefValue);
+
+  const _authEvent = new CustomEvent('authEvent', { detail: hrefValue });
+  document.dispatchEvent(_authEvent);
+}
 
 export default Code;
